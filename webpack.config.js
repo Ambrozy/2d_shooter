838bfe73e5ed969338b3c4e87a633b13207e4005
Ambrozy <bushmanov.vova@yandex.ru>
2021-10-16 15:29:25 +0500

    add ci/cd

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.ts',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'index.bundle.js',
     },
     module: {
